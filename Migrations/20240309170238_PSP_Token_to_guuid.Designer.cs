cf92bb5d2a1e1a332eda0e8d1b0fcc7e0a4ee36a
snjaypee-arch <sn.jaypee@gmail.com>
2024-03-09 18:59:39 -0300

    build(database): change PaymentProvider token from text to GUID

--- Migrations/20240309170238_PSP_Token_to_guuid.Designer.cs
+++ Migrations/20240309170238_PSP_Token_to_guuid.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FazUmPix.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace fazumpix.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240309170238_PSP_Token_to_guuid")]
+    partial class PSP_Token_to_guuid
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240309170238_PSP_Token_to_guuid.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fazumpix.Migrations
+{
+    /// <inheritdoc />
+    public partial class PSP_Token_to_guuid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                ALTER TABLE ""PaymentProvider""
+                ALTER COLUMN ""Token"" TYPE uuid USING ""Token""::uuid
+            ");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Token",
+                table: "PaymentProvider",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Token",
+                table: "PaymentProvider",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+        }
+    }
+}
