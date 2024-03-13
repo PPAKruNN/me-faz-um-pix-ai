7a3dc79dd88bd6e171c8ced336ca24c7722435d8
snjaypee-arch <sn.jaypee@gmail.com>
2024-03-13 20:20:09 -0300

    build(database): add CreatedAt and UpdatedAt to all models

--- Migrations/20240312223106_Add_base_model_to_current_models.Designer.cs
+++ Migrations/20240312223106_Add_base_model_to_current_models.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FazUmPix.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace fazumpix.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240312223106_Add_base_model_to_current_models")]
+    partial class Add_base_model_to_current_models
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20240312223106_Add_base_model_to_current_models.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace fazumpix.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_base_model_to_current_models : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "User",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "User",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "PixKey",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "PixKey",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "PaymentProviderAccount",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "PaymentProviderAccount",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "PaymentProvider",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "PaymentProvider",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "PixKey");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "PixKey");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "PaymentProviderAccount");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "PaymentProviderAccount");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "PaymentProvider");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "PaymentProvider");
+        }
+    }
+}
